cb77a66375fa3fe53cdae27e0c233f3b52a209bb
“Samara <samara.limads@gmail.com>
2023-12-19 12:24:57 +0100

    basicgame logic

--- src/index.js
+++ src/index.js
@@ -5,19 +5,28 @@ let boxes = Array.from(document.getElementsByClassName("box"));
 const playerX = "X";
 const playerO = "O";
 let currentPlayer = " ";
-
-// console.log(boxes);
+let space = Array(9).fill("");
 
 const choosePlayer = () => {
   buttonPlayerX.addEventListener("click", function () {
     currentPlayer = playerX;
-    console.log("X chosen");
+    disablePlayerButtons();
   });
 
   buttonPlayerO.addEventListener("click", function () {
     currentPlayer = playerO;
-    console.log("O chosen");
+    disablePlayerButtons();
   });
+};
+
+const disablePlayerButtons = () => {
+  buttonPlayerX.disabled = true;
+  buttonPlayerO.disabled = true;
+};
+
+const enablePlayerButtons = () => {
+  buttonPlayerX.disabled = false;
+  buttonPlayerO.disabled = false;
 };
 
 const startGame = () => {
@@ -25,10 +34,91 @@ const startGame = () => {
 };
 
 function boxClicked(event) {
-  //   event.target.innerText = currentPlayer;
-  console.log(event.target);
+  const id = event.target.id;
+  if (currentPlayer === " ") {
+    return;
+  }
+  if (!space[id]) {
+    space[id] = currentPlayer;
+    event.target.innerText = currentPlayer;
+    event.target.removeEventListener("click", boxClicked);
+    if (checkForWinner()) {
+      gameOver("Player " + currentPlayer + " is the winner!");
+    } else if (checkForTie()) {
+      gameOver("It's a tie!");
+    } else {
+      togglePlayer();
+    }
+  }
 }
 
-startGame();
+const togglePlayer = () => {
+  if (currentPlayer === playerX) {
+    currentPlayer = playerO;
+  } else {
+    currentPlayer = playerX;
+  }
+};
+
+const checkForWinner = () => {
+  const winPatterns = [
+    [0, 1, 2], // Rows
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // Columns
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // Diagonals
+    [2, 4, 6],
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (space[a] && space[a] === space[b] && space[a] === space[c]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const checkForTie = () => {
+  if (!space.includes("")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const gameOver = (message) => {
+  console.log("Final Board State:", space);
+  setTimeout(() => {
+    showModal(message);
+  }, 150); // Adjust the delay time
+};
+
+const showModal = (message) => {
+  const modal = document.getElementById("customModal");
+  const modalMessage = document.getElementById("modalMessage");
+  const restartButton = document.getElementById("restartButton");
+
+  modalMessage.textContent = message;
+  modal.style.display = "block";
+
+  restartButton.addEventListener("click", () => {
+    modal.style.display = "none";
+    resetGame();
+  });
+};
+
+const resetGame = () => {
+  space = Array(9).fill("");
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.addEventListener("click", boxClicked);
+  });
+
+  enablePlayerButtons();
+};
+
 choosePlayer();
-console.log(currentPlayer);
+startGame();
